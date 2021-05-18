2c5c1baf647559c67ab5c301b7b5f9ea61f8ce3c
mahmudmohsin <72496430+mahmudmohsin@users.noreply.github.com>
2021-05-18 20:57:43 +0600

    Add files via upload

---
+++ aforapple.js
@@ -0,0 +1,464 @@
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var imgA = document.getElementById('myImgA');
+var imgB = document.getElementById('myImgB');
+var imgC = document.getElementById('myImgC');
+var imgD = document.getElementById('myImgD');
+var imgE = document.getElementById('myImgE');
+var imgF = document.getElementById('myImgF');
+var imgG = document.getElementById('myImgG');
+var imgH = document.getElementById('myImgH');
+var imgI = document.getElementById('myImgI');
+var imgJ = document.getElementById('myImgJ');
+var imgK = document.getElementById('myImgK');
+var imgL = document.getElementById('myImgL');
+var imgM = document.getElementById('myImgM');
+var imgN = document.getElementById('myImgN');
+var imgO = document.getElementById('myImgO');
+var imgP = document.getElementById('myImgP');
+var imgQ = document.getElementById('myImgQ');
+var imgR = document.getElementById('myImgR');
+var imgS = document.getElementById('myImgS');
+var imgT = document.getElementById('myImgT');
+var imgU = document.getElementById('myImgU');
+var imgV = document.getElementById('myImgV');
+var imgW = document.getElementById('myImgW');
+var imgX = document.getElementById('myImgX');
+var imgY = document.getElementById('myImgY');
+var imgZ = document.getElementById('myImgZ');
+
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+		let A = document.getElementById("A");
+    	let B = document.getElementById("B");
+    	let C = document.getElementById("C");
+    	let D = document.getElementById("D");
+    	let E = document.getElementById("E");
+    	let F = document.getElementById("F");
+    	let G = document.getElementById("G");
+    	let H = document.getElementById("H");
+    	let I = document.getElementById("I");
+    	let J = document.getElementById("J");
+    	let K = document.getElementById("K");
+    	let L = document.getElementById("L");
+    	let M = document.getElementById("M");
+    	let N = document.getElementById("N");
+    	let O = document.getElementById("O");
+    	let P = document.getElementById("P");
+    	let Q = document.getElementById("Q");
+    	let R = document.getElementById("R");
+    	let S = document.getElementById("S");
+    	let T = document.getElementById("T");
+    	let U = document.getElementById("U");
+    	let V = document.getElementById("V");
+    	let W = document.getElementById("W");
+    	let X = document.getElementById("X");
+    	let Y = document.getElementById("Y");
+    	let Z = document.getElementById("Z");
+    	
+
+
+imgA.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+imgB.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+imgC.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+imgD.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+imgE.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+imgF.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+imgG.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+imgH.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+imgI.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+imgJ.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+imgK.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+imgL.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+imgM.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+imgN.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+imgO.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+imgP.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+imgQ.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+imgR.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+imgS.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+imgT.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+imgU.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+imgV.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+imgW.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+imgX.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+imgY.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+imgZ.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+
+
+A.addEventListener("click", function(){
+    	A.style.background='#39c81f';
+    	const utterance = new SpeechSynthesisUtterance();
+    	utterance.text = "A. A for. apple";
+    	window.speechSynthesis.speak(utterance);
+    	utterance.onend=function(){
+    	A.style.background='#fff';
+    	}
+    	})
+    	B.addEventListener("click", function(){
+    	B.style.background='#39c81f';
+    	const utterance = new SpeechSynthesisUtterance();
+    	utterance.text = "goodbye goodbye! it's end of the day. goodbye, goodbye! now it's time to play.";
+    	window.speechSynthesis.speak(utterance);
+    	utterance.onend=function(){
+    	B.style.background='#fff';
+  		recognition.start();
+    	}
+    	})
+    	C.addEventListener("click", function(){
+    	C.style.background='#39c81f';
+    	const utterance = new SpeechSynthesisUtterance();
+    	utterance.text = "C";
+    	window.speechSynthesis.speak(utterance);
+    	utterance.onend=function(){
+    	C.style.background='#fff';
+    	}
+    	})
+    	D.addEventListener("click", function(){
+    	D.style.background='#39c81f';
+    	const utterance = new SpeechSynthesisUtterance();
+    	utterance.text = "D";
+    	window.speechSynthesis.speak(utterance);
+    	utterance.onend=function(){
+    	D.style.background='#fff';
+    	}
+    	})
+    	E.addEventListener("click", function(){
+    	E.style.background='#39c81f';
+    	const utterance = new SpeechSynthesisUtterance();
+    	utterance.text = "E";
+    	window.speechSynthesis.speak(utterance);
+    	utterance.onend=function(){
+    	E.style.background='#fff';
+    	}
+    	})
+    	F.addEventListener("click", function(){
+    	F.style.background='#39c81f';
+    	const utterance = new SpeechSynthesisUtterance();
+    	utterance.text = "F";
+    	window.speechSynthesis.speak(utterance);
+    	utterance.onend=function(){
+    	F.style.background='#fff';
+    	}
+    	})
+    	G.addEventListener("click", function(){
+    	G.style.background='#39c81f';
+    	const utterance = new SpeechSynthesisUtterance();
+    	utterance.text = "G";
+    	window.speechSynthesis.speak(utterance);
+    	utterance.onend=function(){
+    	G.style.background='#fff';
+    	}
+    	})
+    	H.addEventListener("click", function(){
+    	H.style.background='#39c81f';
+    	const utterance = new SpeechSynthesisUtterance();
+    	utterance.text = "H";
+    	window.speechSynthesis.speak(utterance);
+    	utterance.onend=function(){
+    	H.style.background='#fff';
+    	}
+    	})
+    	I.addEventListener("click", function(){
+    	I.style.background='#39c81f';
+    	const utterance = new SpeechSynthesisUtterance();
+    	utterance.text = "I";
+    	window.speechSynthesis.speak(utterance);
+    	utterance.onend=function(){
+    	I.style.background='#fff';
+    	}
+    	})
+    	J.addEventListener("click", function(){
+    	J.style.background='#39c81f';
+    	const utterance = new SpeechSynthesisUtterance();
+    	utterance.text = "J";
+    	window.speechSynthesis.speak(utterance);
+    	utterance.onend=function(){
+    	J.style.background='#fff';
+    	}
+    	})
+    	K.addEventListener("click", function(){
+    	K.style.background='#39c81f';
+    	const utterance = new SpeechSynthesisUtterance();
+    	utterance.text = "K";
+    	window.speechSynthesis.speak(utterance);
+    	utterance.onend=function(){
+    	K.style.background='#fff';
+    	}
+    	})
+    	L.addEventListener("click", function(){
+    	L.style.background='#39c81f';
+    	const utterance = new SpeechSynthesisUtterance();
+    	utterance.text = "L";
+    	window.speechSynthesis.speak(utterance);
+    	utterance.onend=function(){
+    	L.style.background='#fff';
+    	}
+    	})
+    	M.addEventListener("click", function(){
+    	M.style.background='#39c81f';
+    	const utterance = new SpeechSynthesisUtterance();
+    	utterance.text = "M";
+    	window.speechSynthesis.speak(utterance);
+    	utterance.onend=function(){
+    	M.style.background='#fff';
+    	}
+    	})
+    	N.addEventListener("click", function(){
+    	N.style.background='#39c81f';
+    	const utterance = new SpeechSynthesisUtterance();
+    	utterance.text = "N";
+    	window.speechSynthesis.speak(utterance);
+    	utterance.onend=function(){
+    	N.style.background='#fff';
+    	}
+    	})
+    	O.addEventListener("click", function(){
+    	O.style.background='#39c81f';
+    	const utterance = new SpeechSynthesisUtterance();
+    	utterance.text = "O";
+    	window.speechSynthesis.speak(utterance);
+    	utterance.onend=function(){
+    	O.style.background='#fff';
+    	}
+    	})
+    	P.addEventListener("click", function(){
+    	P.style.background='#39c81f';
+    	const utterance = new SpeechSynthesisUtterance();
+    	utterance.text = "P";
+    	window.speechSynthesis.speak(utterance);
+    	utterance.onend=function(){
+    	P.style.background='#fff';
+    	}
+    	})
+    	Q.addEventListener("click", function(){
+    	Q.style.background='#39c81f';
+    	const utterance = new SpeechSynthesisUtterance();
+    	utterance.text = "Q";
+    	window.speechSynthesis.speak(utterance);
+    	utterance.onend=function(){
+    	Q.style.background='#fff';
+    	}
+    	})
+    	R.addEventListener("click", function(){
+    	R.style.background='#39c81f';
+    	const utterance = new SpeechSynthesisUtterance();
+    	utterance.text = "R";
+    	window.speechSynthesis.speak(utterance);
+    	utterance.onend=function(){
+    	R.style.background='#fff';
+    	}
+    	})
+    	S.addEventListener("click", function(){
+    	S.style.background='#39c81f';
+    	const utterance = new SpeechSynthesisUtterance();
+    	utterance.text = "S";
+    	window.speechSynthesis.speak(utterance);
+    	utterance.onend=function(){
+    	S.style.background='#fff';
+    	}
+    	})
+    	T.addEventListener("click", function(){
+    	T.style.background='#39c81f';
+    	const utterance = new SpeechSynthesisUtterance();
+    	utterance.text = "T";
+    	window.speechSynthesis.speak(utterance);
+    	utterance.onend=function(){
+    	T.style.background='#fff';
+    	}
+    	})
+    	U.addEventListener("click", function(){
+    	U.style.background='#39c81f';
+    	const utterance = new SpeechSynthesisUtterance();
+    	utterance.text = "U";
+    	window.speechSynthesis.speak(utterance);
+    	utterance.onend=function(){
+    	U.style.background='#fff';
+    	}
+    	})
+    	V.addEventListener("click", function(){
+    	V.style.background='#39c81f';
+    	const utterance = new SpeechSynthesisUtterance();
+    	utterance.text = "V";
+    	window.speechSynthesis.speak(utterance);
+    	utterance.onend=function(){
+    	V.style.background='#fff';
+    	}
+    	})
+    	W.addEventListener("click", function(){
+    	W.style.background='#39c81f';
+    	const utterance = new SpeechSynthesisUtterance();
+    	utterance.text = "W";
+    	window.speechSynthesis.speak(utterance);
+    	utterance.onend=function(){
+    	W.style.background='#fff';
+    	}
+    	})
+    	X.addEventListener("click", function(){
+    	X.style.background='#39c81f';
+    	const utterance = new SpeechSynthesisUtterance();
+    	utterance.text = "X";
+    	window.speechSynthesis.speak(utterance);
+    	utterance.onend=function(){
+    	X.style.background='#fff';
+    	}
+    	})
+    	Y.addEventListener("click", function(){
+    	Y.style.background='#39c81f';
+    	const utterance = new SpeechSynthesisUtterance();
+    	utterance.text = "Y";
+    	window.speechSynthesis.speak(utterance);
+    	utterance.onend=function(){
+    	Y.style.background='#fff';
+    	}
+    	})
+    	Z.addEventListener("click", function(){
+    	Z.style.background='#39c81f';
+    	const utterance = new SpeechSynthesisUtterance();
+    	utterance.text = "Z";
+    	window.speechSynthesis.speak(utterance);
+    	utterance.onend=function(){
+    	Z.style.background='#fff';
+    	}
+    	})
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+    modal.style.display = "none";
+}
